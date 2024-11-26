5bef21de104dedb49b408dbabe1b3ec775b047ba
pahmadii <fairyahmadi71@gmail.com>
2024-11-26 19:35:09 +0330

    update src\routes\auth\index.js /added controller require

--- src/routes/auth/index.js
+++ src/routes/auth/index.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const router=express.Router();
-
+const controller=require('./controller');
 
 router.post(
     '/register',
+    controller.register,
 );
 
 router.post(
-    '/login'
+    '/login',
+    controller.login,
 );
 
 

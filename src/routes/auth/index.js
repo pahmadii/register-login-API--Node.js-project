d4b0ffeef0c44fc3014389b1ee7e7671094c225b
pahmadii <fairyahmadi71@gmail.com>
2024-11-26 19:51:55 +0330

    Update src\routes\auth\index.js added require validator ...

--- src/routes/auth/index.js
+++ src/routes/auth/index.js
@@ -1,14 +1,17 @@
 const express = require('express');
 const router=express.Router();
 const controller=require('./controller');
+const validator=require('./validator');
 
 router.post(
     '/register',
+    validator.registerValidator(),
     controller.register,
 );
 
 router.post(
     '/login',
+    validator.loginValidator(),
     controller.login,
 );
 

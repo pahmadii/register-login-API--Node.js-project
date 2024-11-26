dc075d6c39e59bf2681a251df4400a6f33b68490
pahmadii <fairyahmadi71@gmail.com>
2024-11-26 19:25:18 +0330

    Update index.js port

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const router=require('./src/routes/index');
 const app = express();
 
 require('dotenv').config();
@@ -10,6 +11,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(express.static('public'));
 
+app.use('/api',router);
 
 const port = process.env.PORT || 5000;
 app.listen(port, ()=> console.log(`listening on port ${port}`));

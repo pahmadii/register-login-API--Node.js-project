f962f1f95a7ac851d223018a70840cedff828820
pahmadii <fairyahmadi71@gmail.com>
2024-11-27 12:31:28 +0330

    Update  src\routes\controller.js added valitionresult & middleware

--- src/routes/controller.js
+++ src/routes/controller.js
@@ -1,4 +1,6 @@
 const autoBind = require("auto-bind");
+const{validationResult}=require('express-validator');
+
 
 
 module.exports=class{
@@ -6,4 +8,35 @@ module.exports=class{
 constructor(){
 autoBind(this);
 }
+
+
+validationBody(re,res){
+   const result= validationResult(req);
+    if(!result.isEmpty()){
+        const error=result.array();
+        const message=[];
+        error.forEach((err)=>message.push(err.msg));
+        res.status(400).json({
+            message:'vallidator err',
+            data:message
+        })
+        return false;
+       }
+       return true;
+};
+
+validator(req,res,next){
+    if(!this.validationBody(req,res)){
+        return;
+    }
+    next();
+};
+
+response({res,message,code=200,data={}}){
+    res.status(code).json({
+        message,
+        data
+    });
 }
+
+};

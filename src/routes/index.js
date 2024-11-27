6b0034b2cf294f4eb5c00afc9a1474745f618cd8
pahmadii <fairyahmadi71@gmail.com>
2024-11-27 19:36:37 +0330

    Update src\routes\index.js / require error

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,11 +3,16 @@ const router = express.Router();
 const authRouter = require('./auth');
 const userRouter = require('./user');
 const adminRouter = require('./admin');
-const {isLoggined,isAdmin} = require('./../middleware/auth')
+const {isLoggined,isAdmin} = require('./../middleware/auth');
+const error = require('./../middleware/error');
 
 router.use('/auth', authRouter);
 
 router.use('/user', isLoggined, userRouter);
 router.use('/admin', isLoggined, isAdmin, adminRouter);
+
+
+router.use(error);
+
 
 module.exports = router;

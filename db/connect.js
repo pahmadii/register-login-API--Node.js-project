7b3b908341e9497bb08e94a2ad54246817f26e7b
pahmadii <fairyahmadi71@gmail.com>
2024-11-27 13:10:06 +0330

    Update connect.js

--- db/connect.js
+++ db/connect.js
@@ -1,10 +1,11 @@
 const mongoose=require('mongoose');
 
-const connectionString='';
+
+const connectDB=(url)=>{
+   return mongoose
+    .connect(url)
+
+}
 
 
-mongoose.connect()
-.then(()=>{console.log('connected to DB.....')
-})
-.catch((err)=>{console.log(err);
-});
+module.exports=connectDB;
